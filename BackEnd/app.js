9c12cbdea9a4e2547eb0bb36739c63ea26bbd022
Ayush <113465008+ayush1944@users.noreply.github.com>
2025-09-10 03:41:24 -0700

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -11,7 +11,7 @@ import { userRoutes } from './src/routes/user.route.js';
 const app = express();
 
 app.use(cors({
-  origin: 'https://chota-link-nwgu.vercel.app',
+  origin: '*',
   credentials: true 
 }));
 
